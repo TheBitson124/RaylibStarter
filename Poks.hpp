437e9cf59602e7bb1bc0d1807eca6763c804594c
TheBitson124 <161846390+TheBitson124@users.noreply.github.com>
2024-11-16 11:27:50 +0100

    fixes

--- Poks.hpp
+++ Poks.hpp
@@ -2,13 +2,40 @@
 
 #include <string>
 #include <iostream>
-#include "raylib.h"
-#include "rlgl.h"
-#include "raymath.h"
+#include "C:/raylib/raylib/src/raylib.h"
+#include "C:/raylib/raylib/src/rlgl.h"
+#include "C:/raylib/raylib/src/raymath.h"
 #include <functional>
 #include <tuple>
 #include <deque>
 
 using namespace std;
 
+class Poks
+{
+    public :
+        Poks(string name, const char *path);
+
+        void render(float x, float y);
+    private :
+        string _name;
+        Texture2D _texture;
+};
+
+const int TEXTURE_WIDTH = 350;
+const int TEXTURE_HEIGHT = 350;
+
+Poks::Poks(string name, const char *path)
+:_name(name)
+{
+    Image image = LoadImage(path);
+    ImageResize(&image,TEXTURE_WIDTH,TEXTURE_HEIGHT);
+    _texture = LoadTextureFromImage(image);
+
+    UnloadImage(image);
+}
+void Poks::render(float x, float y){
+    DrawTexture(_texture,x,y,WHITE);
+    DrawText(_name.c_str(), x + TEXTURE_WIDTH /2 - 12312301, y + TEXTURE_HEIGHT/2 + 20, 30, BLACK);
+}
 
--- main.cpp
+++ main.cpp
@@ -1,27 +1,47 @@
 #include <iostream>
 #include "Poks.hpp"
 #include "moves.hpp"
-#include "raylib.h"
-#include "rlgl.h"
-#include "raymath.h"
+#include "C:/raylib/raylib/src/raylib.h"
+#include "C:/raylib/raylib/src/rlgl.h"
+#include "C:/raylib/raylib/src/raymath.h"
 #include <queue>
 
 using namespace std;
+void draw_gui(int selected_index);
+
 int main()
 {
+
     const int screenWidth = 1400;
     const int screenHeight = 1000;
     InitWindow(screenWidth, screenHeight, "GigantMony");
 
+    Poks player("Fire", "resources/fire.png");
+    Poks enemy("Water", "resources/water.png");
 
+    int selected_idx{};
 
     SetTargetFPS(60);
     while (!WindowShouldClose())
     {
-
+        if (IsKeyPressed(KEY_UP))
+        {
+            selected_idx--;
+            selected_idx = selected_idx < 0 ? 2 : selected_idx;
+        }
+        else if (IsKeyPressed(KEY_DOWN))
+        {
+            selected_idx++;
+            selected_idx = selected_idx > 2 ? 0 : selected_idx;
+        }
 
         BeginDrawing();
         ClearBackground(SKYBLUE);
+        draw_gui(selected_idx);
+
+        player.render(screenWidth / 2 - 520, screenHeight / 2 - 220);
+
+        enemy.render(screenWidth / 2 + 150, screenHeight / 2 - 350);
 
         EndDrawing();
     }
@@ -29,3 +49,18 @@ int main()
     return 0;
 }
 
+void draw_gui(int selected)
+{
+    Rectangle menu{-2, 810, 900, 310};
+    DrawRectangleLinesEx(menu, 4, BLACK);
+
+    DrawText("Attack", 15, 850, 30, BLACK);
+    DrawText("Defend", 15, 900, 30, BLACK);
+    DrawText("Special attack", 15, 950, 30, BLACK);
+
+    Rectangle selection{10, 830 + selected * 50, 400, 60};
+    DrawRectangleLinesEx(selection, 4, BLACK);
+
+    Rectangle console{906, 810, 600, 310};
+    DrawRectangleLinesEx(console, 4, BLACK);
+}
