c3562a6d3d7d9a7b214c89d092854d733bf8e900
TheBitson124 <161846390+TheBitson124@users.noreply.github.com>
2024-11-23 12:25:29 +0100

    Poks special property

--- Poks.hpp
+++ Poks.hpp
@@ -23,7 +23,7 @@ enum Elements{
 class Poks
 {
     public :
-        Poks(string name,int hp, int ad, int def,Elements el, const char *path);
+        Poks(string name,int hp, int ad, int def,Elements el, const char *path,function<void(Poks &, Poks &, string &)> special);
 
         void render(float x, float y);
 
@@ -50,6 +50,17 @@ class Poks
         bool attack(Poks& target, string& out_string);
         void defend(string& out_string);
         void special(Poks& target, string& out_string);
+        pair<int,int> calculate_attack_dmg(Poks &target);
+
+        inline void change_ad(int change){
+            _ad += change;
+        }
+        inline void change_def(int change){
+            _def += change;
+        }
+        inline void change_hp(int change){
+            _hp += change;
+        }
     private :
         int _base_hp;
         int _base_ad;
@@ -64,6 +75,7 @@ class Poks
 
         string _name;
         Elements _element;
+        function<void(Poks &, Poks &, string &)> special_fun;
 
         Texture2D _texture;
         void draw_hp_bar(float x, float y);
@@ -72,7 +84,7 @@ class Poks
 const int TEXTURE_WIDTH = 350;
 const int TEXTURE_HEIGHT = 350;
 
-Poks::Poks(string name, int hp, int ad, int def,Elements el,const char *path)
+Poks::Poks(string name, int hp, int ad, int def,Elements el,const char *path,function<void(Poks &, Poks &, string &)> special )
 :_name(name)
 {
     _base_hp = hp;
@@ -84,6 +96,7 @@ Poks::Poks(string name, int hp, int ad, int def,Elements el,const char *path)
     _ad = ad;
     _def = def;
 
+    special_fun = special;
     Image image = LoadImage(path);
     ImageResize(&image,TEXTURE_WIDTH,TEXTURE_HEIGHT);
     _texture = LoadTextureFromImage(image);
@@ -99,8 +112,14 @@ void Poks::render(float x, float y){
     draw_hp_bar(x,y);
 }
 bool Poks::attack(Poks& target, string& out_string){
-    target.take_damage(_ad);
-    out_string = this->get_name() + " attacked " + target.get_name()  + " for : " + to_string(_ad);
+    int base{};
+    int bonus{};
+    tie(base,bonus) = calculate_attack_dmg(target);
+
+    int total = base + bonus;
+    target.take_damage(total);
+    out_string = this->get_name() + " attacked " + target.get_name()  + " for : " + to_string(total)
+    +" with bonus of " + to_string(bonus);
     return target.check_if_dead();
 }
 void Poks::defend(string& out_string){
@@ -110,8 +129,7 @@ void Poks::defend(string& out_string){
 
 void Poks::special(Poks& target, string& out_string){
     if(!_used_special){
-        target.take_damage(_ad * 2);
-        out_string = this->get_name() + " used special on " + target.get_name()  + " for : " + to_string(_ad * 2);
+        this->special_fun(*this,target,out_string);
         _used_special = true;
     }
     else{
@@ -130,4 +148,21 @@ void Poks::draw_hp_bar(float x,float y){
     const float fill = (float)_hp/(float)_base_hp;
 
     DrawRectangle(x_anchor + border_width,y_anchor + border_width, fill * (TEXTURE_WIDTH - 10 -2 *border_width),height - 2*border_width, GREEN);
+}
+pair<int,int> Poks::calculate_attack_dmg(Poks &target){
+    int bonus{};
+    auto dif = target._element - this->_element;
+    switch (abs(dif))
+    {
+    case 1:
+        bonus = dif > 0 ? this->_ad *0.5f : this->_ad *-0.5f;
+        break;
+    case Elements::last - 1:
+        bonus = dif > 0 ? this->_ad *-0.5f :this->_ad *0.5f;
+        break;
+    default:
+        bonus = 0;
+        break;
+    }
+    return make_pair(this->_ad,bonus);
 }
